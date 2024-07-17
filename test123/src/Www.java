b9449a9aa60b1bb3951398edf5bf249543b676a3
deba <deba@DESKTOP-F06MU3Q>
2024-07-17 22:18:15 +0530

    test message

--- test123/src/Www.java
+++ test123/src/Www.java
@@ -5,7 +5,7 @@ public class Www {
 	public static void main(String[] args) {
 		// test 123
 		System.out.println("Test Message123456");
-		System.out.println("xyzt");
+		System.out.println("xyzt6");
 
 	}
 
