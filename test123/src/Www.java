08792aeed8b0b10a2766c60c4ff92a55454a9741
deba <deba@DESKTOP-F06MU3Q>
2024-07-17 22:16:52 +0530

    new commit

--- test123/src/Www.java
+++ test123/src/Www.java
@@ -5,7 +5,7 @@ public class Www {
 	public static void main(String[] args) {
 		// test 123
 		System.out.println("Test Message123456");
-		System.out.println("xyz");
+		System.out.println("xyzt");
 
 	}
 
